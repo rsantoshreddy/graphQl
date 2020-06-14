4ebfc13b116f7810642ed81fb2156de62f1eaa51
rsantoshreddy <rsantoshreddy09@gmail.com>
2020-06-14 22:47:23 +0530

    Session-1

---
+++ cars.js
@@ -0,0 +1,8 @@
+﻿const cars = [
+  { id: 1, make: 'Tata', model: 'Rover', owner: 1 },
+  { id: 2, make: 'Tata1', model: 'Rover1', owner: 1 },
+  { id: 3, make: 'Tata2', model: 'Rover2', owner: 2 },
+  { id: 3, make: 'Tata3', model: 'Rover3', owner: 3 },
+];
+
+module.exports = cars;
---
+++ users.js
@@ -0,0 +1,8 @@
+﻿const users = [
+  { id: 1, name: 'Santosh', cars: [1] },
+  { id: 2, name: 'Santosh1', cars: [2] },
+  { id: 3, name: 'Santosh2', cars: [3] },
+  { id: 4, name: 'Santosh3', cars: [4] },
+];
+
+module.exports = users;
